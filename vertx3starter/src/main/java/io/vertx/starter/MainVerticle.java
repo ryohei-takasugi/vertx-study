c877058a10ba9755686c5c2f1b5522b1587a5327
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2022-06-19 15:00:08 +0900

    [add] vert.x web & logger

--- vertx3starter/src/main/java/io/vertx/starter/MainVerticle.java
+++ vertx3starter/src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,24 +1,39 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.http.*;
+import io.vertx.ext.web.*;
+import io.vertx.starter.handle.BodyHandler;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
-  private final static Logger logger = LoggerFactory.getLogger(MainVerticle.class);
-  private final static int port = 8080;
+
+  /**
+   * logger
+   */
+  private final static Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  /**
+   * http port number
+   */
+  private final static int PORT = 8080;
+
+  /**
+   * メイン処理
+   * @param startPromise
+   * @return null
+   */
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> {
-          logger.trace("");
-          logger.debug("");
-          logger.info("");
-          logger.warn("");
-          logger.error("");
-          req.response().end("Hello Vert.x!" + port);
-        })
-        .listen(port);
+  public void start(Promise<Void> startPromise) throws Exception {
+    LOGGER.info("app start");
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+    router.get("/").produces("application/json").handler(BodyHandler.create());
+    server.requestHandler(router).listen(PORT);
+    LOGGER.info("listen start. port: " + PORT);
   }
 
 }
