02a594dd4b162f07f5800b2bb3dc643fd8a306e4
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2022-11-06 01:47:04 +0000

    [update] class comment update

--- vertx1/app/src/main/java/jp/sample/vertx1/ClientServices/Models/NicoNicoModel.java
+++ vertx1/app/src/main/java/jp/sample/vertx1/ClientServices/Models/NicoNicoModel.java
@@ -9,7 +9,100 @@ import java.util.Map;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 
-/** This is the data model for video search results on Nico Nico Douga. */
+/**
+ * This is the data model for video search results on Nico Nico Douga.
+ *
+ * <p>success responce
+ *
+ * <p>
+ *
+ * <table>
+ *   <thead>
+ * .   <tr>
+ *       <th>フィールド名</th>
+ *       <th>型</th>
+ *       <th>例</th>
+ *       <th>説明</th>
+ *     </tr>
+ *   </thead>
+ *   <tbody>
+ *     <tr>
+ *       <th>meta</th>
+ *       <th>object</th>
+ *       <th>-</th>
+ *       <th>レスポンスのメタ情報フィールド</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.status</th>
+ *       <th>integer</th>
+ *       <th>200</th>
+ *       <th>HTTPステータス（成功の場合200）</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.id</th>
+ *       <th>object</th>
+ *       <th>594513df-85ea-4122-9859-f4ec2701cacf</th>
+ *       <th>リクエストID</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.totalCount</th>
+ *       <th>integer</th>
+ *       <th>12673</th>
+ *       <th>ヒット件数</th>
+ *     </tr>
+ *     <tr>
+ *       <th>data</th>
+ *       <th>array</th>
+ *       <th>-</th>
+ *       <th>ヒットしたコンテンツ。要素の内容はパラメータfieldsによって異なります</th>
+ *     </tr>
+ *   </tbody>
+ * </table>
+ *
+ * error responce
+ *
+ * <p>
+ *
+ * <table>
+ *   <thead>
+ * .   <tr>
+ *       <th>フィールド名</th>
+ *       <th>型</th>
+ *       <th>例</th>
+ *       <th>説明</th>
+ *     </tr>
+ *   </thead>
+ *   <tbody>
+ *     <tr>
+ *       <th>meta</th>
+ *       <th>object</th>
+ *       <th>-</th>
+ *       <th>レスポンスのメタ情報フィールド</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.status</th>
+ *       <th>integer</th>
+ *       <th>400</th>
+ *       <th>HTTPステータス（エラーの場合200以外）</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.errorCode</th>
+ *       <th>string</th>
+ *       <th>QUERY_PARSE_ERROR</th>
+ *       <th>エラーコード</th>
+ *     </tr>
+ *     <tr>
+ *       <th>meta.errorMessage</th>
+ *       <th>string</th>
+ *       <th>query parse error</th>
+ *       <th>エラー内容</th>
+ *     </tr>
+ *   </tbody>
+ * </table>
+ *
+ * @see https://site.nicovideo.jp/search-api-docs/snapshot
+ *     <p>
+ */
 public class NicoNicoModel {
 
   /** Logger */
