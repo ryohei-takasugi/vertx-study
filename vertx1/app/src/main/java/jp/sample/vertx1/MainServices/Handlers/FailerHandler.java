f0ac8e8c6bf69b1f3b6b39d410296c11e8d024b9
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2023-03-19 03:10:57 +0000

    [update] add failed handling when error html file can not be loaded.

--- vertx1/app/src/main/java/jp/sample/vertx1/MainServices/Handlers/FailerHandler.java
+++ vertx1/app/src/main/java/jp/sample/vertx1/MainServices/Handlers/FailerHandler.java
@@ -1,6 +1,8 @@
 package jp.sample.vertx1.MainServices.Handlers;
 
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.file.FileSystem;
 import io.vertx.ext.web.RoutingContext;
 import jp.sample.vertx1.share.MyLogger;
 
@@ -9,6 +11,9 @@ public class FailerHandler implements Handler<RoutingContext> {
   /** Logger */
   private static final MyLogger LOGGER = MyLogger.create(FailerHandler.class);
 
+  private static final String File_404_PATH = "error/404.html";
+
+  private static final String File_500_PATH = "error/500.html";
 
   /**
    * Create FailerHandler class method.
@@ -26,10 +31,34 @@ public class FailerHandler implements Handler<RoutingContext> {
    */
   @Override
   public void handle(RoutingContext event) {
-    if (event.statusCode() == 404) {
-      event.response().sendFile("error/404.html");
-    } else {
-      event.response().sendFile("error/500.html");
+    final FileSystem fileSystem = event.vertx().fileSystem();
+    switch (event.statusCode()) {
+      case 404:
+        final Future<Boolean> exists404File = fileSystem.exists(File_404_PATH);
+        exists404File
+            .onSuccess(
+                ex -> {
+                  event.response().setStatusCode(404).sendFile(File_404_PATH);
+                })
+            .onFailure(
+                th -> {
+                  LOGGER.error(event.session(), "", new IllegalAccessError("404 not found"));
+                  event.response().setStatusCode(404).end("404 not found");
+                });
+        break;
+      default:
+        final Future<Boolean> exists500File = fileSystem.exists(File_500_PATH);
+        exists500File
+            .onSuccess(
+                ex -> {
+                  event.response().setStatusCode(500).sendFile(File_500_PATH);
+                })
+            .onFailure(
+                th -> {
+                  LOGGER.error(event.session(), "", new IllegalAccessError("500 server error"));
+                  event.response().setStatusCode(500).end("500 server error");
+                });
+        break;
     }
   }
 }
